0bd965afc258f114d498c686b8c16bc23ffa2290
Jonatan Missora <jonatanjmissora@gmail.com>
2025-04-01 10:24:34 -0300

    checking si hago cambios en el repositorio, se refleja en netlify

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -6,10 +6,11 @@ export default function page() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <div className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <form action={revalidateTagAction}>
-          <button>revalidateTag</button>
+        <form className="flex flex-col gap-2" action={revalidateTagAction}>
+          <h2 className="text-2xl">Form :</h2>
+          <button className="border p-3">revalidateTag</button>
         </form>
-        <Link href={"/"}>Volver</Link>
+        <Link className="border p-3" href={"/"}>Volver</Link>
       </div>
     </div>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,10 @@ export default async function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h1 className="text-3xl">Home</h1>
+        <h1 className="text-3xl font-bold">Home Page</h1>
+        <h2 className="text-2xl">Notes: </h2>
         <NoteList />
-        <Link href={"/form"}>Mutation</Link>
+        <Link className="border p-3" href={"/form"}>Mutation</Link>
       </main>
     </div>
   );
