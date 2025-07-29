7c511ca04c6a4927cabc1be8329fb8af8ab2101d
Jonatan Missora <jonatanjmissora@gmail.com>
2025-07-29 11:50:33 -0300

    refactorizamos y dejamos mas lindo el ui

--- app/data/get-notes.ts
+++ app/data/get-notes.ts
@@ -1,4 +1,4 @@
-import "server-only"
+"use server"
 import { unstable_cache } from "next/cache"
 import { getCollection } from "../lib/db/connect"
 
@@ -6,7 +6,12 @@ export const getNotes = unstable_cache(async () => {
   await new Promise(resolve => setTimeout(resolve, 3000))
   console.log("GET new notes")
   const notesCollection = await getCollection("notes")
-  return await notesCollection.find().toArray()
+  const notes = await notesCollection.find().toArray()
+  // Convert MongoDB documents to plain objects and ensure _id is a string
+  return notes.map(note => ({
+    ...note,
+    _id: note._id.toString(),
+  }))
 },
   ["notes"],
   {
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,38 @@
 import Link from "next/link";
-import NoteList from "./components/NoteList";
+import { Suspense } from "react";
+import { getNotes } from "./data/get-notes";
 
 export default async function Home() {
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <div className="container mx-auto w-8xl">
+      <main className="flex flex-col items-start gap-6">
         <h1 className="text-3xl font-bold">Home Page</h1>
         <h2 className="text-2xl">Notes: </h2>
-        <NoteList />
-        <Link className="border p-3" href={"/form"}>Mutation</Link>
+        <Link className="border rounded-lg p-3" href={"/form"}>Mutation</Link>
+        <Suspense fallback={<p>Loading...</p>}>
+          <NoteList />
+        </Suspense>
       </main>
     </div>
   );
+}
+
+async function NoteList() {
+
+  const notes = await getNotes()
+
+  return (
+    <div className="flex flex-col w-full">
+      {notes.map((note: any) => (
+        <div key={note._id} className="text-lg p-2 flex justify-between gap-2 w-full">
+          <span className="font-bold">title:</span> {note.title}
+          <span className="font-bold">content:</span> {note.content}
+          <span className="font-bold">author:</span> {note.author}
+          <span className="font-bold">pinned:</span> {note.pinned ? "Pinned" : "Not Pinned"}
+          <span className="font-bold">id:</span> {note._id}
+        </div>
+      ))}
+    </div>
+  )
 }
